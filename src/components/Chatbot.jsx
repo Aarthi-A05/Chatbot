b675a4570adda1a3e41cc41097f708fbd0713897
Aarthi A <aarthi.adhikesavan@gmail.com>
2025-05-09 16:41:57 +0530

    backlayout change in chatbot.jsx

--- src/components/Chatbot.jsx
+++ src/components/Chatbot.jsx
@@ -7,7 +7,7 @@ const Chatbot = () => {
 
   return (
     <div className="fixed bottom-4 right-4 z-50">
-      <div className="w-[360px] h-[640px] rounded-3xl shadow-[0_20px_50px_rgba(0,0,0,0.6)] flex flex-col overflow-hidden border border-gray-300 relative bg-transparent">
+      <div className="w-[360px] h-[640px] rounded-3xl flex flex-col overflow-hidden border border-gray-300 backdrop-blur-md bg-white/80 shadow-xl">
         <ChatHeader isInputFocused={isInputFocused} />
         <ChatBody setIsInputFocused={setIsInputFocused} isInputFocused={isInputFocused} />
       </div>
