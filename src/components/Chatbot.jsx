c493e8a5bb3dffe90ca0b1440ef6ba06ebcaf91a
Aarthi A <aarthi.adhikesavan@gmail.com>
2025-05-09 14:03:07 +0530

    bg-transparent in chatbot.jsx

--- src/components/Chatbot.jsx
+++ src/components/Chatbot.jsx
@@ -7,7 +7,7 @@ const Chatbot = () => {
 
   return (
     <div className="fixed bottom-4 right-4 z-50">
-      <div className="w-[360px] h-[640px] rounded-3xl shadow-[0_20px_50px_rgba(0,0,0,0.6)] flex flex-col overflow-hidden border border-gray-300 relative bg-white">
+      <div className="w-[360px] h-[640px] rounded-3xl shadow-[0_20px_50px_rgba(0,0,0,0.6)] flex flex-col overflow-hidden border border-gray-300 relative bg-transparent">
         <ChatHeader isInputFocused={isInputFocused} />
         <ChatBody setIsInputFocused={setIsInputFocused} isInputFocused={isInputFocused} />
       </div>
