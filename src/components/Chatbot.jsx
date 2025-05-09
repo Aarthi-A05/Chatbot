6b0eb324d4eb669fe629c2f86d6795473fd4419b
Aarthi A <aarthi.adhikesavan@gmail.com>
2025-05-09 13:50:07 +0530

    add background index.css

--- src/components/Chatbot.jsx
+++ src/components/Chatbot.jsx
@@ -6,12 +6,10 @@ const Chatbot = () => {
   const [isInputFocused, setIsInputFocused] = useState(false);
 
   return (
-    <div className="min-h-screen bg-transparent">
-      <div className="fixed bottom-4 right-4 z-50">
-        <div className="w-[360px] h-[640px] rounded-3xl shadow-[0_20px_50px_rgba(0,0,0,0.6)] flex flex-col overflow-hidden border border-gray-300 relative">
-          <ChatHeader isInputFocused={isInputFocused} />
-          <ChatBody setIsInputFocused={setIsInputFocused} isInputFocused={isInputFocused} />
-        </div>
+    <div className="fixed bottom-4 right-4 z-50">
+      <div className="w-[360px] h-[640px] rounded-3xl shadow-[0_20px_50px_rgba(0,0,0,0.6)] flex flex-col overflow-hidden border border-gray-300 relative bg-white">
+        <ChatHeader isInputFocused={isInputFocused} />
+        <ChatBody setIsInputFocused={setIsInputFocused} isInputFocused={isInputFocused} />
       </div>
     </div>
   );
